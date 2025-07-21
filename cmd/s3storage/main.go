50723e3730c8e0269a64de9183b46c81849713f0
IX <borntokill.ix@gmail.com>
2025-07-21 22:29:32 +0700

    feat(cmd/s3storage/main.go): add simple listing and download first file from S3 bucket
    It's raw programm

---
+++ cmd/s3storage/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"log"
+	"s3storage/internal/s3"
+	"s3storage/pkg/configs"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+)
+
+const b = 10
+
+func main() {
+	env := configs.NewEnvironment()
+
+	level := env.AppConfig.LogLevel
+	_ = configs.NewLogger(level)
+	// logger.Debug("%+v", "AppConfig:", env.AppConfig)
+	// logger.Debug("%+v", "AwsConfig:", env.AwsConfig)
+
+	s3 := s3.NewS3Client(s3.AwsConfig{
+		Endpoint:   env.AwsConfig.Endpoint,
+		AccessKey:  env.AwsConfig.AccessKey,
+		SecretKey:  env.AwsConfig.SecretKey,
+		Region:     env.AwsConfig.Region,
+		BucketName: env.AwsConfig.BucketName,
+		OutputPath: env.AwsConfig.OutputPath,
+	})
+	listBuckets := s3.ListBucket()
+
+	log.Println("first page results")
+	for _, object := range listBuckets {
+		log.Printf("key=%s size=%d\n", aws.ToString(object.Key), *object.Size)
+	}
+	log.Printf("Total Count objects: %d", len(listBuckets))
+
+	file := *listBuckets[0].Key
+	log.Printf("Load file %s from S3", file)
+	s3.DownloadFile(file)
+}
