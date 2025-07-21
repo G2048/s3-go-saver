5fe1bff1f75bfdcb7c7f346ed5023ba7635e270f
IX <borntokill.ix@gmail.com>
2025-07-21 22:25:19 +0700

    feat(pkg/configs/logs.go): add config for logger

---
+++ pkg/configs/logs.go
@@ -0,0 +1,45 @@
+package configs
+
+import (
+	"log/slog"
+	"os"
+	"strings"
+)
+
+// type Logslevel struct {
+// 	debug slog.Level
+// 	info  slog.Level
+// 	warn  slog.Level
+// 	error slog.Level
+// }
+
+var LogLevels = [4]string{"debug", "info", "warn", "error"}
+var logLevels = map[string]slog.Level{
+	"debug": slog.LevelDebug,
+	"info":  slog.LevelInfo,
+	"warn":  slog.LevelWarn,
+	"error": slog.LevelError,
+}
+
+func NewLogger(level string) *slog.Logger {
+	level = strings.ToLower(level)
+	addSource := false
+	if level == "debug" {
+		addSource = true
+	}
+	options := slog.HandlerOptions{
+		AddSource: addSource,
+		Level:     logLevels[level],
+	}
+	// buildInfo, _ := debug.ReadBuildInfo()
+	var Logger = slog.New(slog.NewJSONHandler(os.Stderr, &options)).With(
+		slog.String("app", "yandex_storage"),
+		slog.String("version", "1.0.0"),
+		// slog.Group("program_info",
+		// slog.Int("pid", os.Getpid()),
+		// slog.String("go_version", buildInfo.GoVersion),
+		// ),
+	)
+	slog.SetDefault(Logger)
+	return Logger
+}
