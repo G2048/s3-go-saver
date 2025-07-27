4c392ba4e5e01928caef27714f24f61ddfe20518
IX <borntokill.ix@gmail.com>
2025-07-27 22:19:48 +0700

    feat(pkg/tui/keymap.go): add the ListKeyMap structure with basic keys: NextTab, PrevTab, HelpMenu, Exit

---
+++ pkg/tui/keymap.go
@@ -0,0 +1,31 @@
+package tui
+
+import "github.com/charmbracelet/bubbles/key"
+
+type ListKeyMap struct {
+	NextTab  key.Binding
+	PrevTab  key.Binding
+	HelpMenu key.Binding
+	Exit     key.Binding
+}
+
+func NewListKeyMap() *ListKeyMap {
+	return &ListKeyMap{
+		Exit: key.NewBinding(
+			key.WithKeys("ctrl+c", "ctrl+d", "q", "esc"),
+			key.WithHelp("q", "exit"),
+		),
+		NextTab: key.NewBinding(
+			key.WithKeys("right", "l", "tab"),
+			key.WithHelp("->/l/tab", "move to right tab"),
+		),
+		PrevTab: key.NewBinding(
+			key.WithKeys("left", "h", "p", "shift+tab"),
+			key.WithHelp("l", "move to left tab"),
+		),
+		HelpMenu: key.NewBinding(
+			key.WithKeys("H"),
+			key.WithHelp("H", "toggle help"),
+		),
+	}
+}
