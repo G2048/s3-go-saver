4bee465aea7854f49433d80436754974bb8034a8
IX <borntokill.ix@gmail.com>
2025-08-01 23:42:24 +0700

    refactor(pkg/tui/items.go): add expliciet field Top and Desc to the Item{} structure

--- pkg/tui/items.go
+++ pkg/tui/items.go
@@ -2,43 +2,40 @@ package tui
 
 import (
 	"github.com/charmbracelet/bubbles/list"
-	"github.com/charmbracelet/lipgloss"
 )
 
-var docStyle = lipgloss.NewStyle().Margin(1, 2)
-
 type Item struct {
-	title, desc string
+	Top, Desc string
 }
 
-func (i Item) Title() string       { return i.title }
-func (i Item) Description() string { return i.desc }
-func (i Item) FilterValue() string { return i.title }
+func (i Item) Title() string       { return i.Top }
+func (i Item) Description() string { return i.Desc }
+func (i Item) FilterValue() string { return i.Top }
 
 func NewItems() []list.Item {
 	return []list.Item{
-		Item{title: "Raspberry Pi’s", desc: "I have ’em all over my house"},
-		Item{title: "Nutella", desc: "It's good on toast"},
-		Item{title: "Bitter melon", desc: "It cools you down"},
-		Item{title: "Nice socks", desc: "And by that I mean socks without holes"},
-		Item{title: "Eight hours of sleep", desc: "I had this once"},
-		Item{title: "Cats", desc: "Usually"},
-		Item{title: "Plantasia, the album", desc: "My plants love it too"},
-		Item{title: "Pour over coffee", desc: "It takes forever to make though"},
-		Item{title: "VR", desc: "Virtual reality...what is there to say?"},
-		Item{title: "Noguchi Lamps", desc: "Such pleasing organic forms"},
-		Item{title: "Linux", desc: "Pretty much the best OS"},
-		Item{title: "Business school", desc: "Just kidding"},
-		Item{title: "Pottery", desc: "Wet clay is a great feeling"},
-		Item{title: "Shampoo", desc: "Nothing like clean hair"},
-		Item{title: "Table tennis", desc: "It’s surprisingly exhausting"},
-		Item{title: "Milk crates", desc: "Great for packing in your extra stuff"},
-		Item{title: "Afternoon tea", desc: "Especially the tea sandwich part"},
-		Item{title: "Stickers", desc: "The thicker the vinyl the better"},
-		Item{title: "20° Weather", desc: "Celsius, not Fahrenheit"},
-		Item{title: "Warm light", desc: "Like around 2700 Kelvin"},
-		Item{title: "The vernal equinox", desc: "The autumnal equinox is pretty good too"},
-		Item{title: "Gaffer’s tape", desc: "Basically sticky fabric"},
-		Item{title: "Terrycloth", desc: "In other words, towel fabric"},
+		Item{Top: "Raspberry Pi’s", Desc: "I have ’em all over my house"},
+		Item{Top: "Nutella", Desc: "It's good on toast"},
+		Item{Top: "Bitter melon", Desc: "It cools you down"},
+		Item{Top: "Nice socks", Desc: "And by that I mean socks without holes"},
+		Item{Top: "Eight hours of sleep", Desc: "I had this once"},
+		Item{Top: "Cats", Desc: "Usually"},
+		Item{Top: "Plantasia, the album", Desc: "My plants love it too"},
+		Item{Top: "Pour over coffee", Desc: "It takes forever to make though"},
+		Item{Top: "VR", Desc: "Virtual reality...what is there to say?"},
+		Item{Top: "Noguchi Lamps", Desc: "Such pleasing organic forms"},
+		Item{Top: "Linux", Desc: "Pretty much the best OS"},
+		Item{Top: "Business school", Desc: "Just kidding"},
+		Item{Top: "Pottery", Desc: "Wet clay is a great feeling"},
+		Item{Top: "Shampoo", Desc: "Nothing like clean hair"},
+		Item{Top: "Table tennis", Desc: "It’s surprisingly exhausting"},
+		Item{Top: "Milk crates", Desc: "Great for packing in your extra stuff"},
+		Item{Top: "Afternoon tea", Desc: "Especially the tea sandwich part"},
+		Item{Top: "Stickers", Desc: "The thicker the vinyl the better"},
+		Item{Top: "20° Weather", Desc: "Celsius, not Fahrenheit"},
+		Item{Top: "Warm light", Desc: "Like around 2700 Kelvin"},
+		Item{Top: "The vernal equinox", Desc: "The autumnal equinox is pretty good too"},
+		Item{Top: "Gaffer’s tape", Desc: "Basically sticky fabric"},
+		Item{Top: "Terrycloth", Desc: "In other words, towel fabric"},
 	}
 }
