8c61c7c4f0e29d5a0306315bf2ffd68defd7a69a
IX <borntokill.ix@gmail.com>
2025-08-03 13:19:04 +0700

    feat(cmd/tui/main.go): change list of content inside tui to list of s3 bucket entities

--- cmd/tui/main.go
+++ cmd/tui/main.go
@@ -3,13 +3,30 @@ package main
 import (
 	"fmt"
 	"os"
+	"s3storage/configs"
+	"s3storage/internal/adapters"
+	"s3storage/internal/s3"
 	"s3storage/pkg/tui"
 
 	tea "github.com/charmbracelet/bubbletea"
 )
 
 func main() {
-	m := tui.TestModelTabs()
+	env := configs.NewEnvironment()
+	s3 := s3.NewS3Client(s3.AwsConfig{
+		Endpoint:   env.AwsConfig.Endpoint,
+		AccessKey:  env.AwsConfig.AccessKey,
+		SecretKey:  env.AwsConfig.SecretKey,
+		Region:     env.AwsConfig.Region,
+		BucketName: env.AwsConfig.BucketName,
+	})
+	adapter := adapters.S3ListItems{S3: s3}
+	items := *adapter.ListBucket()
+	fmt.Printf("%#v\n", items)
+
+	tabs := []string{"Buckets", "Files"}
+	m := tui.NewModelTabs(tabs, items)
+	// m := tui.TestModelTabs()
 	if _, err := tea.NewProgram(m, tea.WithAltScreen()).Run(); err != nil {
 		fmt.Println("Error running program:", err)
 		os.Exit(1)
