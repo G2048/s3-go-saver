39c4805cca49609d9121d55ee152dd2da263526f
IX <borntokill.ix@gmail.com>
2026-01-07 00:04:03 +0700

    feat(internal/s3/files.go): add S3Client.UploadFiles()

--- internal/s3/files.go
+++ internal/s3/files.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"io/fs"
 	"log/slog"
 	"os"
 	"path/filepath"
@@ -30,6 +31,45 @@ func (client *S3Client) UploadFile(fileName string) error {
 	if err != nil {
 		slog.Error(fmt.Sprintf("Couldn't upload file %v to S3. Here's why: %v", fileName, err))
 	}
+	return err
+}
+func (client *S3Client) UploadFiles(dirName string) error {
+	var err error
+	err = filepath.WalkDir(dirName, func(path string, entry fs.DirEntry, err error) error {
+		if err != nil {
+			slog.Error(fmt.Sprintf("Couldn't open dir %s to upload. Here's why: %v", path, err))
+			return err
+		}
+
+		fullPath, err := filepath.Abs(path)
+		if err != nil {
+			slog.Error(fmt.Sprintf("Couldn't open dir %s by full path to upload. Here's why: %v", fullPath, err))
+			return err
+		}
+
+		if entry.Type().IsRegular() {
+			slog.Info(fmt.Sprintf("Load into S3 the %s file...", path))
+			file, err := os.Open(fullPath)
+			if err != nil {
+				slog.Error(fmt.Sprintf("Couldn't open file %s to upload. Here's why: %v", fullPath, err))
+				return err
+			}
+			defer file.Close()
+
+			_, err = client.s3.PutObject(context.TODO(), &s3.PutObjectInput{
+				Bucket: aws.String(client.BucketName),
+				Key:    aws.String(path),
+				Body:   file,
+			})
+			if err != nil {
+				slog.Error(fmt.Sprintf("Couldn't upload file %v to S3. Here's why: %v", fullPath, err))
+				return err
+			}
+
+		}
+		return nil
+	})
+
 	return err
 }
 
