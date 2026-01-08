275c34daec30bb3e497211f79defb57e93759548
IX <borntokill.ix@gmail.com>
2026-01-08 22:37:02 +0700

    test(remove unnecessary calls of File sturcture: Close and Open):

--- pkg/synchronizer/synchronizer_test.go
+++ pkg/synchronizer/synchronizer_test.go
@@ -23,15 +23,14 @@ func TestFile(t *testing.T) {
 	fmt.Println("Write info to file1")
 	file1.Write([]byte("Record1: Hello World in file1!\n"))
 	file1.Write([]byte("Record2: Hello World in file1!\n"))
-	file1.Close()
 
 	fmt.Println("Write info to file2")
 	file2.Write([]byte("Record1: Hello World in file2!\n"))
 	file2.Write([]byte("Record2: Hello World in file2!\n"))
 
-	file1.Open()
 	fileBody1 := file1.ReadFull()
-	fmt.Printf("File body: %s\n", fileBody1)
+	fmt.Printf("\nFile body:\n %s\n", fileBody1)
+
 	fileBody2 := file2.ReadFull()
-	fmt.Printf("File body: %s\n", fileBody2)
+	fmt.Printf("\nFile body:\n %s\n", fileBody2)
 }
